5db82e350a1b131805def3204fd8c989859d88a7
Paulo Camara <paulo.camara@qintess.com>
2021-10-24 00:29:34 -0300

    Separating component from loader

--- src/components/contexts/AxiosInterceptor/AxiosInterceptor.js
+++ src/components/contexts/AxiosInterceptor/AxiosInterceptor.js
@@ -1,6 +1,6 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
+import { LoaderContainer, Loader } from '../../shared/Loader/Loader';
 import { api } from "../../../scripts/Request";
-import styled from "styled-components";
 import toastr from "toastr";
 
 export const AxiosInterceptors = ({ children }) => {
@@ -39,7 +39,7 @@ export const AxiosInterceptors = ({ children }) => {
             return Promise.reject(error);
         }
     );
-    
+
     return (
         <React.Fragment>
             {isLoading && <LoaderContainer>
@@ -49,31 +49,3 @@ export const AxiosInterceptors = ({ children }) => {
         </React.Fragment>
     );
 };
-
-
-const LoaderContainer = styled.div`
-    background-color: #000000;
-    opacity: 0.7;
-    position: fixed;
-    height: 100%; 
-    width: 100%;
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-    top: 0;
-`;
-const Loader = styled.div`
-    width: 100px;
-    height: 100px;
-    border: 7px solid #FFFFFF;
-    border-top-color: #ee6b2f;
-    border-radius: 50%;
-    animation: spin 1s linear infinite;
-
-    @keyframes spin {
-        to {
-            transform: rotate(360deg);
-        }
-    }
-`;
